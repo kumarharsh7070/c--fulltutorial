91fc95477d0cc88bea45abbda237fcc3e8c9d306
kumar harsh <hk995814@gmail.com>
2025-06-12 20:05:38 +0530

    Initial commit with my C learning files with basic program

---
+++ input.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+// int main() {
+//     int age;
+//     printf("enter age - ");
+//     scanf("%d", &age);
+//     printf("age is : %d", age);
+// }
+
+int main(){
+    int num1;
+    int num2;
+    int sum;
+    printf("Enter first numer - ");
+    scanf("%d", &num1);
+    printf("Enter second number - ");
+    scanf("%d", &num2);
+    sum= num1 + num2;
+    printf("sum of num1 and num2 - %d", sum);
+    return 0;
+}
